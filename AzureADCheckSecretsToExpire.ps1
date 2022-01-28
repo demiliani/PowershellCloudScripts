8d09dc3301c7a5958f08d352d573544bb2a3a4b5
Stefano Demiliani <33155438+demiliani@users.noreply.github.com>
2022-01-28 11:17:15 +0100

    Added Azure AD client secret expiration check script
    Added script for checking Azure AD app registration secrets that expires in a range of period.

---
+++ AzureADCheckSecretsToExpire.ps1
@@ -0,0 +1,40 @@
+﻿Connect-AzureAD
+
+$LimitExpirationDays = 31 #secret expiration date filter
+
+#Retrieving the list of secrets that expires in the above days
+$SecretsToExpire = Get-AzureADApplication -All:$true | ForEach-Object {
+    $app = $_
+    @(
+        Get-AzureADApplicationPasswordCredential -ObjectId $_.ObjectId
+        Get-AzureADApplicationKeyCredential -ObjectId $_.ObjectId
+    ) | Where-Object {
+        $_.EndDate -lt (Get-Date).AddDays($LimitExpirationDays)
+    } | ForEach-Object {
+        $id = "Not set"
+        if($_.CustomKeyIdentifier) {
+            $id = [System.Text.Encoding]::UTF8.GetString($_.CustomKeyIdentifier)
+        }
+        [PSCustomObject] @{
+            App = $app.DisplayName
+            ObjectID = $app.ObjectId
+            AppId = $app.AppId
+            Type = $_.GetType().name
+            KeyIdentifier = $id
+            EndDate = $_.EndDate
+        }
+    }
+}
+ 
+#Gridview list
+#$SecretsToExpire | Out-GridView
+
+#Printing the list of secrets that are near to expire
+if($SecretsToExpire.Count -EQ 0) {
+    Write-Output "No secrets found that will expire in this range"
+}
+else {
+    Write-Output "Secrets that will expire in this range:"
+    Write-Output $SecretsToExpire.Count
+    Write-Output $SecretsToExpire
+}
